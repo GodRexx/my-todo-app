10a609148b9c288f6dab8299c866497395d4fa1e
GodRexx <anaytopwal08@gmail.com>
2026-06-18 22:10:54 +0530

    Add initial implementation of todo app with Streamlit
    - Create functions for reading and writing todos
    - Implement main app logic in web_app.py
    - Add todos.txt for storing todo items
    - Include .gitignore to exclude compiled Python files

---
+++ web_app.py
@@ -0,0 +1,32 @@
+# python -m streamlit run web_app.py
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("Hello, Streamlit!")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity")
+
+for index, todo in enumerate(todos):
+    # Use index to guarantee uniqueness
+    checkbox = st.checkbox(todo, key=f"{todo}_{index}")
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[f"{todo}_{index}"]
+        st.rerun()
+
+st.text_input(
+    label="Enter a todo",
+    placeholder="Add new todo..",
+    on_change=add_todo,
+    key="new_todo"
+)
+
+st.session_state
